d8951c6a043ba127d2bb592f3b46199b02900acf
taitruong <me@mr-t.org>
2019-12-10 21:52:24 +0100

    use reactive forms

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,24 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { UsersComponent } from './users/users.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatTableModule } from '@angular/material/table';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { UsersComponent } from "./users/users.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatTableModule, MatFormFieldModule, MatInputModule } from "@angular/material";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    UsersComponent
-  ],
+  declarations: [AppComponent, UsersComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
     MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- client/src/app/users/users.component.ts
+++ client/src/app/users/users.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from './user.model';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material';
 
 const ELEMENT_DATA: User[] = [
+  {firstName: '', lastName: '', email: ''},
   {firstName: 'John', lastName: 'Doe', email: 'john.doe@acme.com'},
   {firstName: 'Alan', lastName: 'Smithee', email: 'alan.smithee@example.com'},
 ];
@@ -14,9 +17,25 @@ const ELEMENT_DATA: User[] = [
 export class UsersComponent implements OnInit {
 
   displayedColumns: string[] = ['id', 'firstName', 'lastName', 'email'];
-  dataSource = ELEMENT_DATA;
+  dataSource: MatTableDataSource<FormGroup>;
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    const userFormGroups = ELEMENT_DATA.map(user => {
+      return fb.group({
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email
+      });
+    });
+    console.log('>>>> form', this.form);
+    this.form = fb.group({
+      users: fb.array(userFormGroups)
+    });
+    this.dataSource = new MatTableDataSource(userFormGroups);
+  }
 
   ngOnInit() {
   }
